1635ddfcf5002846d0fb289d944f3c20cccbf2da
George S. <shipmang8@gmail.com>
2026-05-28 22:27:54 +0900

    Delete build directory

--- build/CMakeFiles/sakura.dir/CORE/PPU_2C02/utils_ppu.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sakura.dir/CORE/PPU_2C02/utils_ppu.cpp.o: \
- /home/Hunter/Documents/Github/sakura/CORE/PPU_2C02/utils_ppu.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/sakura.dir/MAIN/sakura.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sakura.dir/MAIN/sakura.cpp.o: \
- /home/Hunter/Documents/Github/sakura/MAIN/sakura.cpp \
- /usr/include/stdc-predef.h
