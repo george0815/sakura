90277f15fff2338846c35d5f023e413bf7cb8798
george0815 <shipmang8@gmail.com>
2026-05-28 14:50:05 +0900

    updated some of the ASCII

--- UI/obj/Debug/net10.0/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net10.0/UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ab8bd49577c4a84f700996d3c64ccfbc9d83a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4455004ec5d33ab3926413d46f86ff7307bc88")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
